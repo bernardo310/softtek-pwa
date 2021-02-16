069c8e8bee1b9bc502f88ac84aa93733dd3ae2a8
Mauricio De Leon <mauricio.deleon.c13@gmail.com>
2021-02-16 14:33:49 -0600

    Added style to signup

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,6 +1,9 @@
 import React, { useRef, useState } from 'react'
 import { useAuth } from '../contexts/AuthContext';
-import { Link, useHistory } from 'react-router-dom'
+import { Link, useHistory } from 'react-router-dom';
+import { Container, Row, Col } from 'react-bootstrap';
+import Input from './common/Input';
+import Button from './common/Button';
 
 export default function Signup() {
     const emailRef = useRef();
@@ -28,21 +31,39 @@ export default function Signup() {
     }
 
     return (
-        <div>
-            {error ? <p>{error}</p> : ''}
-            {JSON.stringify(currentUser)}
-            <form onSubmit={handleSubmit}>
-                <p>correo</p>
-                <input type="text" ref={emailRef} />
-                <p>password</p>
-                <input type="text" ref={passwordRef} />
-                <p>confirm password</p>
-                <input type="text" ref={confirmpasswordRef} />
-                <button type="submit" disabled={loading}>registrar</button>
-            </form>
-            <div>
-                Ya tengo cuenta <Link to="/login"> </Link>
-            </div>
-        </div>
+        <Container className='vertical-center full-height'>
+            <Row className='justify-content-center'>
+                <Col xs={12} md={5} className='text-center'>
+                    <h1>Plaza Real Order To Go</h1>
+                    <h2>Crear cuenta</h2>
+                </Col>
+            </Row>
+            <Row className='justify-content-center'>
+                <Col xs={12} md={3}>
+                    {error ? <p>{error}</p> : ''}
+                    {/*{JSON.stringify(currentUser)}*/}
+                    <form onSubmit={handleSubmit}>
+                        {/*<p>correo</p>
+                        <input type="text" ref={emailRef} />
+                        <p>password</p>
+                        <input type="text" ref={passwordRef} />
+                        <p>confirm password</p>
+                        <input type="text" ref={confirmpasswordRef} />
+                        <button type="submit" disabled={loading}>registrar</button>*/}
+
+                        <Input type='email' label='Correo' ref={emailRef} />
+                        <Input type='password' label='Contraseña' ref={passwordRef} />
+                        <Input type='password' label='Confirmar contraseña' ref={confirmpasswordRef} />
+                        <Button type='submit' disabled={loading} label='Registrarme' variant='primary'/>
+                    </form>
+                </Col>
+            </Row>
+            <Row className='justify-content-center'>
+                <Col xs={12} md={3} className='text-center'>
+                    <hr />
+                    <p className='text-smallest'>¿Ya tienes cuenta? <Link to="/login"> Inicia sesión</Link></p>
+                </Col>
+            </Row>
+        </Container>
     )
 }
